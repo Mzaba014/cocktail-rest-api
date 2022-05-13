4fa0f717ca56ad4a0bc87a3562d9e23b58527160
Manuel Zabala <37668464+Mzaba014@users.noreply.github.com>
2022-05-13 15:03:18 -0600

    Wrote tests for user credential validation. Implemented super user creation.

--- app/core/models.py
+++ app/core/models.py
@@ -5,16 +5,43 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
     """provides helper functions for creating users or super users"""
 
-    def create_user(self, email, password):
-        user = self.model(email=email)
+    def normalize_email(self, email: str) -> str:
+        """Normalize email by lower-casing the domain part"""
+        index = email.index('@')
+        username = email[:index]
+        domain = email[index:].lower()
+
+        return username + domain
+
+    def create_user(self, email: str, password: str):
+        """Returns an instance of the custom User model using the given params"""
+        if email is None:
+            raise ValueError("Email cannot be None")
+        elif password is None:
+            raise ValueError("Password cannot be None")
+
+        user = self.model(email=self.normalize_email(email))
         user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email: str, password: str):
+        """Leverage create_user to create a User, then modify the permission booleans and return the modified User"""
+        user = self.create_user(email, password)
+        print(type(user))
+        user.is_superuser = True # Boolean provided by PermissionsMixin
+        user.is_admin = True
         user.save()
 
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model which allows email as username"""
+    """Custom User model created by extending AbstractBaseUser and PermissionsMixin.
+
+       ABU provides core implementation of User model such as hashed passwords
+       PermissionsMixin provides db fields and methods for Django's permission model"""
     email = models.EmailField(max_length=254, unique=True)
     objects = UserManager()
     USERNAME_FIELD = 'email'  # the name of the field in the user model that is used as the unique identifier
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,6 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     def test_create_user(self):
         """Test creation of a user with an email as the username, a custom functionality defined in our User model"""
-
         email = 'test@manny.dev'
         password = 'test_pass'
         user = get_user_model().objects.create_user(
@@ -16,3 +15,33 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(
             user.check_password(password))  # must use provided check_pass helper bc passwords are auto-hashed
+
+    def test_create_superuser(self):
+        """Test creation of a User with is_superuser and is_admin, aka a SuperUser"""
+        email = 'test@manny.dev'
+        password = 'test_pass'
+
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_admin)
+
+    def test_email_normalization(self):
+        """Test that the domain segment of the email is normalized when new user is created"""
+        email = 'test@MANNY.DEV'
+        user = get_user_model().objects.create_user(email, 'test_pass')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_email_validation(self):
+        """Test that creating a user with no email raises an exception"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test_pass')
+
+    def test_password_validation(self):
+        """Test that creating a user with no password raises an exception"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('test@manny.dev', None)
